6dcec06fe2a1353bef42b1540661a52eac810264
mpnam1991 <bkier09@gmail.com>
2011-11-29 23:26:29 +0700

    complete base site. You can visit our website with header and footer. kakaka

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-	return render_to_response('home.html', context_instance=RequestContext(request))
+	return render_to_response('home.html', {'title': 'Home'}, context_instance=RequestContext(request))
 	
 def category(request, name):
 	pass
