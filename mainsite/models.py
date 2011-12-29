48f4b0a392fd98a5e5f2ec6f56814c1dba03a0fd
Clapika <clapika2010@gmail.com>
2011-12-29 16:29:30 +0700

    Update

--- mainsite/models.py
+++ mainsite/models.py
@@ -15,12 +15,14 @@ class Ebook (models.Model):
 	comments=models.ManyToManyField('UserInfo',through='Comment',related_name='ebook_comment')
 	uploader=models.ForeignKey('UserInfo',related_name='ebook_uploader')
 	slug=models.SlugField(unique=True)
-	download_link=models.URLField()
 	image_link=models.URLField()
 	authors=models.CharField(max_length=200)
+	
 	@models.permalink
 	def get_absolute_url():
-		return ('library.mainsite.views.view',self.slug)
+		return ('library.mainsite.views.view',self.slug)	
+	def __unicode__(self):
+		return self.name
 	def get_model_name(self):
 		return self.__class__.__name__ 
 	
@@ -41,18 +43,23 @@ class Category(models.Model):
 	@models.permalink
 	def get_absolute_url():
 		return ('library.mainsite.views.category',self.slug)
+	def __unicode__(self):
+		return self.name
 	def get_model_name(self):
 		return self.__class__.__name__ 
 		
 class Subject(models.Model):
+	CHOICES=(('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'))
 	name=models.CharField(max_length=100,primary_key=True)
 	subject_info=models.CharField(max_length=300)
-	semester=models.IntegerField()
+	semester=models.IntegerField(choices=CHOICES)
 	slug=models.SlugField(unique=True)
 	
 	@models.permalink
 	def get_absolute_url():
 		return ('library.mainsite.views.subject',self.slug)
+	def __unicode__(self):
+		return self.name
 	def get_model_name(self):
 		return self.__class__.__name__ 
 	
@@ -60,6 +67,13 @@ class UserInfo(models.Model):
 	user=models.ForeignKey(User)
 	user_info=models.CharField(max_length=300)
 	download_list=models.ManyToManyField(Ebook,through='Download')
+	
+	class Meta:
+		verbose_name_plural="User Infos"
+		
+	def __unicode__(self):
+		return self.user.username
+		
 User.profile = property(lambda u: PubProfile.objects.get_or_create(user=u)[0])
 	
 
@@ -69,24 +83,23 @@ class Download(models.Model):
 	download_time=models.DateField(default=datetime.date.today())
 
 class Level(models.Model):
+	CHOICES=(('1','Beginner'),('2','Intermediate'),('3','Advanced'))
 	category=models.ForeignKey(Category)
 	ebook=models.ForeignKey(Ebook)
 	user=models.ForeignKey(UserInfo)
-	level=models.IntegerField()	
+	level=models.IntegerField(choices=CHOICES)	
 	
 class Comment(models.Model):
-	user=models.ForeignKey(UserInfo)
 	ebook=models.ForeignKey(Ebook)
+	user=models.ForeignKey(UserInfo)
 	content=models.TextField()
 	
 class Rate(models.Model):
+	CHOICES=(('1','1'),('2','2'),('3','3'),('4','4'),('5','5'))
 	user=models.ForeignKey(UserInfo)
 	ebook=models.ForeignKey(Ebook)
-	rate=models.IntegerField()
+	rate=models.IntegerField(choices=CHOICES)
 	
-class EbookAdmin(admin.ModelAdmin):
-	exclude=('slug',)
-	prepopulated_fields={'slug':['name']}
 
 
 
